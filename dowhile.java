d0d9f56c0d48ca5da11fd2bd35011017da3d3364
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:12:13 +0530

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.print(i+" ");
+			i++;
+		}while(i <=10);
+		
+		
+		System.out.println("");
+		
+		
+		i=10;
+		do {
+			System.out.print(i+" ");
+			//i--;
+		}while(--i>0);
+
+
+	}
+
+}
