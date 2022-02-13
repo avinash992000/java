18b988b90eca5916b016a99db1f42aee760e025e
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:17:25 +0530

    Create fordemo.java

---
+++ fordemo.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println(" ");
+		for(int i=10;i>0;i--) {
+			System.out.print(i+" ");
+		}
+		System.out.println(" ");
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println("2 x " + i + " = " + 2*i);
+		}
+		System.out.println("out of the for loop");
+
+	}
+
+}
