b1face5da2b60353667fa1b8ed552c937ae85d5e
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 21:52:11 +0530

    Create stringfromuser.java

---
+++ stringfromuser.java
@@ -0,0 +1,12 @@
+// You are using Java
+import java.util.Scanner;
+
+class InputString{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string value");
+        System.out.println("");
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
