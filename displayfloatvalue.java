f5a99a4be62822f8c918f79ad8aaebd05d733b54
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 21:50:38 +0530

    Create displayfloatvalue.java

---
+++ displayfloatvalue.java
@@ -0,0 +1,9 @@
+// You are using Java
+class display{
+    public static void main(String[] args){
+        float n = 45000.0f;
+        double m = 250000.0;
+        System.out.println(n);
+        System.out.println(m);
+    }
+}
