5ad5cbc0a2f766530bb61a514cec88eb4f500c5c
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:08:49 +0530

    Create types.java

---
+++ types.java
@@ -0,0 +1,33 @@
+package day1;
+
+public class types {
+
+	public static void main(String[] args) {
+		int number = 10;
+		double d=56.78;
+		double e=50.78;
+		float num=17.87f;
+		char c ='a';
+		boolean flag = true;
+		
+		int n=70;
+		int sum =number+n;
+		
+		int age;
+		
+		age = 100;
+		
+		System.out.println("Sum is " + (d+e));
+		System.out.println("character is " + c);
+		System.out.println("result for n-num "+ (n-num));
+		
+		// increment operators --a,a--,++a,a++
+		int a=5;
+		int b=6;
+		b=a++;// post increment ++a pre increment
+		System.out.println(b);
+		
+
+	}
+
+}
