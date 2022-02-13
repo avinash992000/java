a8818db66eaf1b1e5c5e2a050c35cd2cbe2070ef
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:07:28 +0530

    Create positiveno.java

---
+++ positiveno.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Positiveno {
+
+	public static void main(String[] args) {
+		int a =Integer.parseInt(args[0]);
+		System.out.println(a);
+		
+		if(a>=0) {
+			System.out.println("positive number");
+		}
+		else {
+			System.out.println("negative number");
+		}
+
+	}
+
+}
