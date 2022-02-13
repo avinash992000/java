fa907f72d247710d2061d40f2fb8fb47b7b166b3
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:22:50 +0530

    Create switchcases.java

---
+++ switchcases.java
@@ -0,0 +1,40 @@
+package day2;
+
+public class SwitchCases {
+
+	public static void main(String[] args) {
+		int month = 4;
+		switch(month) {
+			case 1: System.out.println("january");
+			break;
+			case 2: System.out.println("feb");
+			break;
+			case 3: System.out.println("march");
+			break;
+			case 4: System.out.println("april");
+			break;
+			case 5: System.out.println("may");
+			break;
+			case 6: System.out.println("june");
+			break;
+			case 7: System.out.println("july");
+			break;
+			case 8: System.out.println("august");
+			break;
+			case 9: System.out.println("september");
+			break;
+			case 10: System.out.println("october");
+			break;
+			case 11: System.out.println("nov");
+			break;
+			case 12: System.out.println("december");
+			break;
+			default:System.out.println("incorrect");
+		
+		}
+		System.out.println("out of the switch case");
+		
+
+	}
+
+}
