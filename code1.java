c03309e92a7cba40814e78c3ed3cb6a3124bd62e
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:03:09 +0530

    Create code1.java

---
+++ code1.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class code1 {
+
+	public static void main(String[] args) {
+		System.out.println("------------------------");
+		System.out.println("Name\tAge\tDoB");
+		System.out.println("------------------------");
+		System.out.println("Ram\t32\t07/07/1997");
+		
+
+	}
+
+}
