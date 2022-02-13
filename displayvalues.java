5b3ecd2df35dd51cfa41d44bcb7aa0140b3f5409
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 21:49:55 +0530

    Create displayvalues.java

---
+++ displayvalues.java
@@ -0,0 +1,9 @@
+// You are using Java
+class display{
+    public static void main(String[] args){
+        System.out.println("20");
+        System.out.println("10.33");
+        System.out.println("K");
+        System.out.println("Rohit");
+    }
+}
