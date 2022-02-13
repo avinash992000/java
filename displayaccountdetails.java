9ec527f4a38ba822afd015f26659e15f77f1ee7b
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 21:51:31 +0530

    Create displayaccountdetails.java

---
+++ displayaccountdetails.java
@@ -0,0 +1,9 @@
+// You are using Java
+class AccountDisplay{
+    public static void main(String[] args){
+        System.out.println("Account Number: 192834");
+        System.out.println("Account Active:true");
+        System.out.println("Account Type (S: Savings  C: Current): S");
+        System.out.println("Account Balance: $11423.79");
+    }
+}
