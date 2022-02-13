f0a6458a155a2f508c18bdaf93cddd333a1de562
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:25:28 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<10) {
+			System.out.print(i+" ");
+			i++;
+		}
+		System.out.println(" ");
+		
+		i=10;
+		while(i>0) {
+			System.out.print(i+" ");
+			i--;
+		}
+
+	}
+
+}
