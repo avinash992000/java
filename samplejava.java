88ea02d3762d605e3016121850d649c36daf5cf8
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:08:10 +0530

    Create samplejava.java

---
+++ samplejava.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class sample {
+
+	public static void main(String[] args) {
+		sample sc = new sample();
+		int result= sc.sum(2, 4);
+		System.out.println(result);
+		
+
+	}
+	public int sum(int a,int b) {
+		return a+b;
+	}
+
+}
