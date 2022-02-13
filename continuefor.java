8b7a9abada49eec25f7f7cedaed709034f772fa7
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:11:37 +0530

    Create continuefor.java

---
+++ continuefor.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class ContinueFor {
+
+	public static void main(String[] args) {
+		for(int j=10;j>0 ;j--) {
+			if(j==5) {
+				continue;
+			}
+			System.out.print(j+" ");
+		}
+
+	}
+
+}
