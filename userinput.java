51d053ec6715f003aa01fee30d3bbbea1c9c2cda
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-13 22:24:27 +0530

    Create userinput.java

---
+++ userinput.java
@@ -0,0 +1,13 @@
+package day2;
+import java.util.Scanner;
+public class UserInput {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String num = sc.next();
+		System.out.println(num);
+		sc.close();
+
+	}
+
+}
