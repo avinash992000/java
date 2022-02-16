2a7cd008dcd6a056e7be77e6914cb336a3cf4058
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 22:21:30 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class StudentDay4 {
+	private int id;
+	private String name;
+	
+	public void Values(int id,String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public void display() {
+		System.out.println("id: "+id+"name: "+name);
+	}
+
+}
