bfb2a99dedafa84ff5c475823d3cf96700e6f85f
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 22:05:18 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,46 @@
+package day5;
+public class AccountDay4 {
+private long AccountNo;
+private String name;
+private String email;
+private float Amount;
+public long getAccountNo() {
+return AccountNo;
+}
+public void setAccountNo(long accountNo) {
+AccountNo = accountNo;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getEmail() {
+return email;
+}
+public void setEmail(String email) {
+this.email = email;
+}
+public float getAmount() {
+return Amount;
+}
+public void setAmount(float amount) {
+Amount = amount;
+}
+public void display() {
+System.out.println("AccountNo: "+getAccountNo()+"Name: "+getName()+"Email: 
+"+getEmail()+"Amount: "+getAmount());
+}
+}
+package day5;
+public class AccountDay4Driver {
+public static void main(String[] args) {
+AccountDay4 Acc1 = new AccountDay4();
+Acc1.setAccountNo(23456);
+Acc1.setName("Avinash");
+Acc1.setEmail("Sample@gmail.com");
+Acc1.setAmount(1000000);
+Acc1.display();
+}
+}
