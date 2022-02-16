6d6d098e9957e4c57ee6f830456da8869b584d5c
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 21:50:47 +0530

    Create employee1.java

---
+++ employee1.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class EmployeeDay5 {
+	private float Salary;
+
+	public float getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(float salary) {
+		Salary = salary;
+	}
+	
+	
+
+}
