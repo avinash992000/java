45e82338a463a1da17f578d3b509fd6ca1b05aec
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 21:46:20 +0530

    Create animal.java

---
+++ animal.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class AnimalDay4 {
+	private String name;
+	private String name1;
+	
+	AnimalDay4(){
+		
+	}
+	
+	AnimalDay4(String name,String name1) {
+		this.name=name;
+		this.name1=name1;
+	}
+	public void displayanimal() {
+		System.out.print("animal name: "+name+"animal name1"+name1);
+		
+	}
+
+}
