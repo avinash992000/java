8f17128ecee7634b953ce65ad4bf09f8a13fcc8f
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 21:39:14 +0530

    Create address.java

---
+++ address.java
@@ -0,0 +1,43 @@
+package day4;
+
+public class Address {
+	private String AddressLine1;
+	private String AddressLine2;
+	private String city;
+	private String state;
+	private String pincode;
+	
+	public String getAddressLine1() {
+		return AddressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		AddressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return AddressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		AddressLine2 = addressLine2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	
+	
+
+}
