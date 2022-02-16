3a2a42fd459f204ecad744c6f0268546c1d7c733
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 21:45:17 +0530

    Create accountdriver1.java

---
+++ accountdriver1.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class AccountDay4Driver {
+
+	public static void main(String[] args) {
+		AccountDay4 Acc1 = new AccountDay4();
+		Acc1.setAccountNo(23456);
+		Acc1.setName("Avinash");
+		Acc1.setEmail("Sample@gmail.com");
+		Acc1.setAmount(1000000);
+		Acc1.display();
+
+	}
+
+}
