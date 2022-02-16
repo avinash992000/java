03608d41b3487c53a8949229df117e0bf9927a43
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 22:22:00 +0530

    Create studentdriver.java

---
+++ studentdriver.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class StudentDriver {
+
+	public static void main(String[] args) {
+		StudentDay4 Std1 = new StudentDay4();
+		Std1.Values(1, "Avinash");
+		Std1.display();
+
+	}
+
+}
