90f0857b21d74ddb50fcc0325a681dd79e81fcf8
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 21:57:10 +0530

    Create programer.java

---
+++ programer.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class ProgrammerDay5Driver {
+
+	public static void main(String[] args) {
+		ProgrammerDay5 p1 = new ProgrammerDay5();
+		p1.setSalary(250000);
+		p1.setBonus(25000);
+		p1.displayTotalSalary();
+
+	}
+
+}
