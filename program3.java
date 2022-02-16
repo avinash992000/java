4e371f2f22ec73fee2d4648405d4cf083496b66a
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-16 22:08:39 +0530

    Create program3.java

---
+++ program3.java
@@ -0,0 +1,14 @@
+package day5;
+interface BankDay5 {
+void ROI();
+}
+package day5;
+public class SbiDay5 implements BankDay5 {
+public void ROI() {
+System.out.println("Printing from Defined method from SBI");
+}
+public static void main(String[] args) {
+BankDay5 B1 = new SbiDay5();
+B1.ROI();
+}
+}
