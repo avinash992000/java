7d20194a614e4b348c76f428bbe1b1a7811c7095
AVINASH <86355693+avinash992000@users.noreply.github.com>
2022-02-18 20:29:16 +0530

    Update SumNine.java

--- SumNine.java
+++ SumNine.java
@@ -8,19 +8,19 @@ public class SumNine {
         int check = sc.nextInt();
         int num=0;
         int sum = 0;
-        while(check>0) {
-        	num=check;
+        int i = 9;
+        while(i<check) {
+        	num=i;
         	sum=0;
         	while(num>0) {
             	int rem = num%10;
             	sum += rem;
             	num=num/10;
             }
-        	//check--;
         	if(sum==9) {
-        		System.out.println(check);
+        		System.out.println(i);
         	}
-        	check--;
+        	i++;
             
         }
         
